861ef79d6505b42612d5b2b2e619f5390c732b29
Shoucheng H <h.shoucheng@gmail.com>
2017-05-17 14:24:09 +0800

    Add import and template file.

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Home from './containers/home/index.jsx';
+import './static/css/common.less';
+import './static/css/font.css';
+
+
+render(
+    <Home />,
+    document.getElementById('root')
+)
