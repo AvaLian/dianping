8afd3c36c09f8c84ae2b743044cde783f7024723
Shoucheng H <h.shoucheng@gmail.com>
2017-06-08 16:35:31 +0800

    Add comment button and state.

--- app/components/OrderListComponent/Item/index.jsx
+++ app/components/OrderListComponent/Item/index.jsx
@@ -7,24 +7,63 @@ class Item extends React.Component {
   constructor(props, context){
     super(props, context);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state = {
+      commentState: 2
+    }
   }
   render(){
     const data = this.props.data;
     return(
       <div className="clear-fix order-item-container">
-        <div className="order-item-img float-left">
-          <img src={data.img} />
+        <div className="clear-fix">
+          <div className="order-item-img float-left">
+            <img src={data.img} />
+          </div>
+          <div className="order-item-comment float-right">
+            {
+              this.state.commentState === 0
+              ? <button className="btn" onClick={this.showComment}>未评价</button>
+              :
+                this.state.commentState === 1
+                //评价中
+                ? ''
+                //已评价
+                : <button className="btn unselect-btn">已评价</button>
+            }
+          </div>
+          <div className="order-item-content">
+            <span>商户：{data.title}</span>
+            <span>数量：{data.count}</span>
+            <span>价格：¥{data.price}</span>
+          </div>
         </div>
-        <div className="order-item-comment float-right">
-          <button>评价</button>
-        </div>
-        <div className="order-item-content">
-          <span>商户：{data.title}</span>
-          <span>数量：{data.count}</span>
-          <span>价格：¥{data.price}</span>
-        </div>
+        {
+          //"评价中"才会显示输入框
+          this.state.commentState === 1
+          ? <div className="comment-text-container">
+            <textarea style={{width: '100%', height: '80px'}} className="comment-text" ref="commentText"></textarea>
+            <button className='btn' onClick={this.submitComment.bind(this)}>提交</button>
+            <button className="btn unselect-btn" onClick={this.hideComment.bind(this)}>取消</button>
+          </div>
+          : ''
+        }
       </div>
     )
+  }
+  componentDidMount(){
+    this.setState({
+      commentState: this.props.data.commentState
+    })
+  }
+  showComment(){
+    this.setState({
+      commentState: 1
+    })
+  }
+  hideComment(){
+    this.setState({
+      commentState: 0
+    })
   }
 }
 
